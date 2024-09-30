e072a3d6bd260879b39a4ba53b402753b19212a1
Leena Elmashat <lelmasha@usc.edu>
2024-09-29 20:10:32 -0700

    Update datastore.h

--- Online Retail System/datastore.h
+++ Online Retail System/datastore.h
@@ -8,11 +8,6 @@
 
 /**
  * DataStore Interface needed for parsing and instantiating products and users
- *
- * A derived version of the DataStore can provide other services as well but
- * must support those below
- *
- * DO NOT EDIT
  */
 
 
