89229863389f40dcd864f39c9fdef93d530d8346
Leena Elmashat <lelmasha@usc.edu>
2024-09-30 19:49:15 -0700

    Update schedwork-driver.cpp

--- Wordle and Scheduling/schedwork-driver.cpp
+++ Wordle and Scheduling/schedwork-driver.cpp
@@ -1,6 +1,4 @@
-// main() program to test your schedule function.
-// Feel free to modify this program in any way you need for testing.
-// It will not be graded. 
+// main() program to test  schedule function.
 #include <set>
 #include <iostream>
 #include <fstream>
@@ -16,7 +14,6 @@ void printSchedule(const DailySchedule& sched);
 
 int main()
 {
-    // Replace the matrix below with your test case
     AvailabilityMatrix avail = { 
         {1, 1, 1, 1},
         {1, 0, 1, 0},
