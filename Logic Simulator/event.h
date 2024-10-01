7895810256db23164f08127e66634662a713bcb9
Leena Elmashat <lelmasha@usc.edu>
2024-09-30 19:34:03 -0700

    Add files via upload

---
+++ Logic Simulator/event.h
@@ -0,0 +1,19 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include "wire.h"
+
+struct Event {
+    uint64_t time;
+    Wire* wire;
+    char state;
+};
+
+struct EventLess {
+    bool operator()(const Event& lhs, const Event& rhs) const {
+        return lhs.time < rhs.time; // Sorting in descending order for a min-heap
+    }
+};
+
+#endif
+
